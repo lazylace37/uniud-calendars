da2e7d79d6942af14e2c71e037599561dfb6e29e
Gioele Vuaran <vuarangioele@gmail.com>
2023-09-12 14:45:52 +0000

    feat: add .json file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import json
 from pathlib import Path
 from uniud import get_course_lessons, get_year_courses, get_years
 from ics import Calendar, Event
+import urllib.parse
 
 
 def main():
@@ -9,7 +11,14 @@ def main():
 
     latest_year = int(ordered_years[0]["valore"])
 
+    all_courses = {}
+
     for course in get_year_courses(latest_year):
+        if course["label"] not in all_courses:
+            all_courses[course["label"]] = {}
+        if not course["tipo"] in all_courses[course["label"]]:
+            all_courses[course["label"]][course["tipo"]] = {}
+
         for anno in course["elenco_anni"]:
             anno_di_insegnamento: str = anno["valore"].split("|")[1]
 
@@ -31,13 +40,20 @@ def main():
                 if anno_di_insegnamento.isdigit()
                 else f'{anno["label"]}.ics'
             )
-            ical_file_path = Path(
+            ical_file_path_name = (
                 f'ical/{course["label"]}/{course["tipo"]}/{ical_file_name}'
             )
+            ical_file_path = Path(ical_file_path_name)
             ical_file_path.parent.mkdir(exist_ok=True, parents=True)
             with open(str(ical_file_path), "w") as my_file:
                 my_file.writelines(c.serialize_iter())
             # endregion
+
+            link = f"https://raw.githubusercontent.com/lazylace37/uniud-calendars/main/{urllib.parse.quote(ical_file_path_name)}"
+            all_courses[course["label"]][course["tipo"]][anno["label"]] = link
+
+    with open("ical/calendars.json", "w") as f:
+        f.write(json.dumps(all_courses))
 
 
 if __name__ == "__main__":
